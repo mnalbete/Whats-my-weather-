490eb728ce9bea4f80c558cc3c009d4dda69944c
mathew <mnalbete@uw.edu>
2020-11-23 22:24:48 -0800

    11/23/2020 redone work added var to the functionresponse

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,10 @@ $(document).ready(function () {
     var searchedCity = localStorage.getItem("#cityName");
     var API_KEY = "0368fc91275f10ac609c0d8c6957a966"
 
+
     // current weather queryURL
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityEL + '&appid=' + API_KEY;
+
 
     // 5 day queryurl
     var futureURL = 'api.openweathermap.org/data/2.5/forecast?q=' + cityEL + '&appid=' + API_KEY;
@@ -27,8 +29,25 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
 
+      // current city search requirements
+      
+      var cityLat = response.coord.lat;
+      console.log(cityLat);
+      var cityLon = response.coord.lon;
+      console.log(cityLon);
+      var cityName = response.name;
+      console.log(cityName);
+      var cityDate = moment().format("MM/DD/YYYY");
+      console.log(cityDate);
+      var cityTemp = (response.main.temp - 273.15) * 1.80 + 32;
+      console.log(cityTemp);
+      var cityHum = response.main.humidity;
+      console.log(cityHum);
+      var cityWind = response.wind.speed;
+      console.log(cityWind);
+      
       // name of city date and picture
-      // var currentCity = $("<h1>")response.name;
+      // $("#currentCity").html("<h1>", cityName + " " + cityDate + "</h1>");
 
       // tempeture
       // var currentTemp = ;
@@ -42,10 +61,13 @@ $(document).ready(function () {
 
       // Uv index queryurl
 
-      
-      var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + API_KEY;
+
+
 
       // uv index
+
+      // uvURL
+      var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + API_KEY;
       // var currentUV = ;
 
 
