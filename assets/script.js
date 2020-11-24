6162c5bd45e8ae756d561055a81ae823f6b0a99c
mathew <mnalbete@uw.edu>
2020-11-24 01:12:21 -0800

    11/24/2020 updated javascript to append to page

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,13 @@
 $(document).ready(function () {
   var searchBtn = $("#searchButton");
 
-
-
-  // search button within form 
-  searchBtn.on("click", function (event) {
-    event.preventDefault();
-    // setting up var
-    var cityLat;
-    var cityLon;
+  function buildQuaryURL() {
     var cityEL = $("#cityName").val();
     var searchedCity = localStorage.getItem("#cityName");
-    var API_KEY = "0368fc91275f10ac609c0d8c6957a966"
-
+    var API_KEY = "0368fc91275f10ac609c0d8c6957a966";
 
     // current weather queryURL
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityEL + '&appid=' + API_KEY;
-
-
-    // 5 day queryurl
-    var futureURL = 'api.openweathermap.org/data/2.5/forecast?q=' + cityEL + '&appid=' + API_KEY;
-
 
     // ajax 
     $.ajax({
@@ -30,7 +17,7 @@ $(document).ready(function () {
       console.log(response);
 
       // current city search requirements
-      
+
       var cityLat = response.coord.lat;
       console.log(cityLat);
       var cityLon = response.coord.lon;
@@ -39,61 +26,74 @@ $(document).ready(function () {
       console.log(cityName);
       var cityDate = moment().format("MM/DD/YYYY");
       console.log(cityDate);
+      var cityIcon = response.weather[0].icon;
+      console.log(cityIcon);
       var cityTemp = (response.main.temp - 273.15) * 1.80 + 32;
       console.log(cityTemp);
       var cityHum = response.main.humidity;
       console.log(cityHum);
       var cityWind = response.wind.speed;
       console.log(cityWind);
+      cityUVURL(cityLat, cityLon);
+
+      // for(var i = 0; i < cityIcon, i++){
+
+      // };
       
-      // name of city date and picture
-      // $("#currentCity").html("<h1>", cityName + " " + cityDate + "</h1>");
 
-      // tempeture
-      // var currentTemp = ;
-
-      // humidity
-      // var currentHum = ;
-
-      // wind speed
-      // var currentWSp = ;
-
-
-      // Uv index queryurl
-
-
-
-
-      // uv index
-
-      // uvURL
-      var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + API_KEY;
-      // var currentUV = ;
-
+      $("#searchedcityName").text(cityName); 
+      $("#searchedcityDate").text(cityDate);
+      $("#searchedcityIcon").attr("src","http://openweathermap.org/img/w/" + cityIcon + ".png");
+      // temp hum and wind
+      $("#searchedcityTemp").text(cityTemp);
+      $("#searchedcityHum").text(cityHum);
+      $("#searchedcityWind").text(cityWind);
+    
 
 
     });
 
+  };
+
+  function cityUVURL(cityLat, cityLon) {
+    var API_KEY = "0368fc91275f10ac609c0d8c6957a966";
+    var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + API_KEY;
+
+    $.ajax({
+      url: uvURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      var cityUV = response.value;
+      // uv
+      $("#searchedcityUV").text(cityUV)
+    });
+  };
+
+
+  function fivedayURL() {
+    // 5 day queryurl
+    var cityEL = $("#cityName").val();
+    var API_KEY = "0368fc91275f10ac609c0d8c6957a966";
+    var futureURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityEL + '&appid=' + API_KEY;
+
+    $.ajax({
+      url: futureURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    });
+
+  };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // search button within form 
+  searchBtn.on("click", function (event) {
+    event.preventDefault();
+    buildQuaryURL();
+    fivedayURL();
   });
-
 
 
 
